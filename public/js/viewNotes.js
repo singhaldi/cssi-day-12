18e65425eb217f046e0963c77a0b3b315b79ef98
Divya <singhaldivya11@gmail.com>
2021-07-27 17:07:30 +0000

    notes app working

--- public/js/viewNotes.js
+++ public/js/viewNotes.js
@@ -22,8 +22,25 @@ const getNotes = (userId) => {
 
 const renderDataAsHtml = (data) => {
     let cards = ``;
-    for (const noteItem in data){
+    for (const noteItem in data) {
         const note = data[noteItem];
         console.log(`${note.title} ${note.text}`)
+        cards += createCard(note);
     }
+    document.querySelector("#app").innerHTML = cards;
+}
+
+const createCard = (note) => {
+    return `
+        <div class="column-is-one-quarter">
+            <div class="card">
+                <header class="card-header">
+                    <p class="card-header-title">${note.title}</p>
+                </header>
+                <div class="card-content">
+                    <div class="content">${note.text}</div>
+                </div>
+            </div>
+        </div>
+    `;
 }
